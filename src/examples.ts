7ac4dbba54ae7d6b252570584415ed6dff4360a9
Carlos Luis Quesada <carlosluisquesada@gmail.com>
2020-02-14 23:03:50 -0500

    Second pass at ugcep flow

--- src/examples.ts
+++ src/examples.ts
@@ -1,8 +1,6 @@
-import { assign } from 'xstate';
-
 const machine = {
-  id: 'chooseImage',
-  initial: 'showing_toc',
+  id: 'addYourOwnContent',
+  initial: 'selecting_image',
   context: {
     retries: 0,
     image_received: false
@@ -11,71 +9,102 @@ const machine = {
     EXIT: '.exit'
   },
   states: {
-    showing_toc: {
-      on: {
-        ACCEPT_TOC: 'ready_to_select_image'
-      }
-    },
     exit: {
       type: 'final'
     },
-    ready_to_select_image: {
-      on: {
-        WITHDRAW_TOC: 'showing_toc',
-        UPLOAD_IMAGE: [
-          {
-            cond: { type: 'image_is_valid' },
-            target: 'uploading'
-          },
-          {
-            target: '.invalid_image_selected',
-            actions: ['increment_retries']
-          }
-        ]
-      },
-      initial: 'ready',
+    selecting_image: {
+      initial: 'waiting_for_toc_acceptance',
       states: {
-        ready: {},
-        // client-side validation
-        invalid_image_selected: {}
-      }
-    },
-    uploading: {
-      on: {
-        NETWORK_ERROR: [
-          {
-            cond: 'customer_is_stuck',
-            target: 'upload_failed.showing_retry_and_skip'
-          },
-          {
-            target: 'upload_failed'
-          }
-        ],
-        // TODO Optional proceed to customizer without UGC. proceed_without_image
-        SERVER_REJECTION: 'ready_to_select_image.invalid_image_selected',
-        RECEIVED: {
-          target: 'selecting_style',
-          actions: ['mark_image_received']
-        },
-      }
-    },
-    // network failure or server-side validation failure
-    upload_failed: {
-      on: {
-        RETRY: {
-          target: 'uploading',
-          actions: ['increment_retries']
-        }
-      },
-      initial: 'showing_retry',
-      states: {
-        showing_retry: {},
-        showing_retry_and_skip: {
+        waiting_for_toc_acceptance: {
+          id: 'waiting_for_toc_acceptance',
           on: {
+            ACCEPT_TOC: '#ready_to_select_image'
+          }
+        },
+        ready_to_select_image: {
+          id: 'ready_to_select_image',
+          on: {
+            WITHDRAW_TOC: '#waiting_for_toc_acceptance',
+            UPLOAD_IMAGE: [
+              {
+                cond: { type: 'image_is_valid' },
+                target: '#uploading_image'
+              },
+              {
+                target: '#invalid_image_selected',
+                actions: ['increment_retries']
+              }
+            ]
+          }
+        },
+        // client-side validation
+        invalid_image_selected: {
+          id: "invalid_image_selected",
+          on: {
+            UPLOAD_IMAGE: [
+              {
+                cond: { type: 'image_is_valid' },
+                target: '#uploading_image'
+              },
+              {
+                target: '#invalid_image_selected',
+                actions: ['increment_retries']
+              }
+            ]
+          }
+        }
+      }
+    },
+    uploading_image: {
+      id: 'uploading_image',
+      initial: 'sending',
+      states: {
+        sending: {
+          on: {
+            NETWORK_ERROR: [
+              {
+                cond: 'customer_is_stuck',
+                target: '#upload_failed_again'
+              },
+              {
+                target: '#upload_failed'
+              }
+            ],
+            SERVER_REJECTION: [
+              {
+                cond: 'customer_is_stuck',
+                target: '#addYourOwnContent.selecting_image.invalid_image_selected'
+              },
+              {
+                target: '#addYourOwnContent.selecting_image.invalid_image_selected'
+              }
+            ],
+            RECEIVED: {
+              target: '#selecting_style',
+              actions: ['mark_image_received']
+            },
+          }
+        },
+        upload_failed: {
+          id: "upload_failed",
+          on: {
+            RETRY: {
+              target: 'sending',
+              actions: ['increment_retries']
+            }
+          },
+        },
+        upload_failed_again: {
+          id: "upload_failed_again",
+          on: {
+            RETRY: {
+              target: 'sending',
+              actions: ['increment_retries']
+            },
             SKIP: {
               target: '#selecting_style'
             }
-          }
+          },
         }
       }
     },
@@ -88,17 +117,17 @@ const machine = {
             target: 'selecting_size'
           },
           {
-            target: 'proceed'
+            target: 'moving_to_customizer'
           }
         ]
       }
     },
     selecting_size: {
       on: {
-        SELECT_SIZE: 'proceed'
+        SELECT_SIZE: 'moving_to_customizer'
       }
     },
-    proceed: {
+    moving_to_customizer: {
       initial: 'unknown',
       states: {
         unknown: {
@@ -123,12 +152,11 @@ const machine = {
 const options = `{
   guards: {
     image_is_valid: function image_is_valid (_, event) {
-      console.log('event.valid', event.valid);
       return event.invalid === undefined;
     },
     customer_is_stuck: function customer_is_stuck (context) {
       console.log('context.retries', context.retries);
-      return context.retries > 1;
+      return context.retries > 0;
     },
     style_has_size: function style_has_size (_, event) {
       return event.has_styles === true;
